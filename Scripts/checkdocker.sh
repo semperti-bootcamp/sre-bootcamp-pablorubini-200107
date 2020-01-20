412d251681458606ccd5706ee1b556bdb140a7ba
Pablo Rubini <pablorubini@gmail.com>
2020-01-20 20:35:34 -0300

    Update checkdocker.sh

--- Scripts/checkdocker.sh
+++ Scripts/checkdocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker container ps -aq -f ancestor=pablitorub/journals | xargs -t docker container stop
-if [$? == 0]; then
+if [$? -eq 0]; then
     docker container ps -aq -f ancestor=pablitorub/journals | xargs docker rm
 else
     echo "container does not exists..."
